74604d3b2474d3f49f40aadfd8b8ffcbc1340f73
Alyssa Holland <aholland@riskalyze.com>
2022-11-21 01:05:06 -0500

    Install React Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from "react-router-dom";
 import App from './App'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -7,6 +8,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 )
