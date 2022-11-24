9ad04ed9468dbfbcc7f991701a2ad52d0c09b80c
Alyssa Holland <aholland@riskalyze.com>
2022-11-24 00:01:41 -0500

    Update NavHeader component

--- src/components/NavHeader.jsx
+++ src/components/NavHeader.jsx
@@ -1,21 +1,34 @@
 import React from 'react'
-import logo from '../assets/logo.svg'
+import logo from '../assets/logo-dark.svg'
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Badge from 'react-bootstrap/Badge';
 
 export default function NavHeader() {
     return (
-        <Navbar bg="light" variant="light">
+        <Navbar bg="dark" variant="dark">
             <Container fluid>
-                <img
-                    alt="Medusa logo"
-                    src={logo}
-                    width="150"
-                />
-                <Navbar.Text className="text-dark">
-                    {/**TODO: Wrap text in a React Router <Link /> tag */}
-                    🛍️ Products
-                </Navbar.Text>
+                <Nav className="w-25 d-flex align-items-center justify-content-between">
+                    <img
+                        alt="Medusa logo"
+                        src={logo}
+                        width="150"
+                    />
+
+                    <Navbar.Text className="text-light">
+                        {/**TODO: Wrap text in a React Router <Link /> tag */}
+                        🛍️ Products
+                    </Navbar.Text>
+                </Nav>
+                <Nav>
+                    <Navbar.Text className="text-light">
+                        {/**TODO: Wrap text in a React Router <Link /> tag */}
+                        Cart
+                        <Badge bg="success" className="rounded-circle ms-2">9</Badge>
+                        <span className="visually-hidden">number of items in the cart</span>
+                    </Navbar.Text>
+                </Nav>
             </Container>
         </Navbar>
     )
