719e4f676d67c6d67216e208f4612742a6d6f654
Alyssa Holland <aholland@riskalyze.com>
2022-11-26 03:45:56 -0500

    Install and Configure the Medusa JS Client

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    // Medusa uses CORS to only allow specific origins to access the server. By default, it looks at port 8000.
+    //https://docs.medusajs.com/usage/configurations/#storefront-cors
+    port: 8000,
+  }
 })
